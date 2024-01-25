cceefbbf03e0002649db89563f0c8fa4fd905baa
Yazid <ymekhtoub@gmail.com>
2024-01-24 22:26:12 -0500

    added prisma

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # delete all containers and images 
 
-# docker rm -vf $(docker ps -aq)
-# docker rmi -f $(docker images -aq)
+docker rm -vf $(docker ps -aq)
+docker rmi -f $(docker images -aq)
 
 # build a container
 docker build . -t dash
--- dev.sh
+++ dev.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # adding bash at the end to override the docker CMD
-docker run -it --name dev dash bash
+
+docker rm -vf $(docker ps -aq)
+
+docker run -it -p 5432:5432 --name dev dash bash
+
+
 
 
